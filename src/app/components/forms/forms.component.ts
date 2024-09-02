64224ea23c94c84a835d36511d8b754719427d3d
Rodrigo-db1 <rodrigo.castro@db1.com.br>
2024-09-02 11:25:27 -0300

    feat/filtro-acertado/service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,23 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 import { Location } from 'src/app/types/location.interface';
 
-const OPENING_HOURS = {
-  morning: {
-    first:'06',
-    last:'12'
-  },
-  afternoon: {
-    first:'12',
-    last:'18'
-  },
-  night: {
-    first:'18',
-    last:'23'
-  }  
-}
-type HOUR_INDEXES = 'morning' | 'afternoon' | 'night'
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -29,12 +15,12 @@ export class FormsComponent implements OnInit {
     formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, 
-               private unitService: GetUnitsService
+               private unitService: GetUnitsService,
+               private  filterUnitsService: FilterUnitsService
                ) { }
 
   ngOnInit(): void 
-  {    
-   
+  {       
     this.formGroup = this.formBuilder.group({
       hour:'',
       showClosed: true
@@ -44,26 +30,12 @@ export class FormsComponent implements OnInit {
       this.filteredResults=data.locations;
     });     
   }  
-    filterUnits(unit: Location, opened: boolean, open_hour: string, close_hour: string){
-      let open_hour_filter = parseInt(open_hour,10)
-      let close_hour_filter = parseInt(close_hour,10)
-
-      
-    }
-
+  
     onSubmit(): void
      {
-      const OPEN_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first
-      const CLOSE_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last
-
-        if(!this.formGroup.value.showClosed)
-        {
-          this.filteredResults = this.results.filter(location => location.opened ===true)
-        } else {
-          this.filteredResults = this.results;
-        }
-     }
-  
+      let {showClosed, hour} = this.formGroup.value
+      this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour)  
+     } 
 
    onClean(): void{
      this.formGroup.reset();
